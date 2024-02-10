d0ea3cef4a7744d62b53716fe43a1392ab0d0982
Herik Mitre <33600632+jHerikmp@users.noreply.github.com>
2024-02-10 20:09:32 -0300

    first

---
+++ index.js
@@ -0,0 +1,57 @@
+function chama_cliente(){
+    document.getElementById("demo").innerHTML = ''
+    nome_cliente = document.getElementById('nome_cliente').value
+    pesquisa_por_nome(nome_cliente)    
+  }
+  
+  async function consulta_dados_cliente(){
+    const dados = await fetch('./dados_cliente.json')    
+    let data = await dados.json();     
+    return data
+  }
+
+  async function pesquisa_por_nome(nome_cliente){    
+    let data = await consulta_dados_cliente(); 
+    const array_dados_clientes = JSON.parse(JSON.stringify(data));
+    let encontrou_cliente = []
+    array_dados_clientes.forEach(element => {
+        if(element.nome_cliente.toLowerCase().includes(nome_cliente.toLowerCase())){
+            encontrou_cliente.push(element)
+        }
+    });    
+    if(encontrou_cliente.length > 0){
+        monta_tabela(encontrou_cliente)
+    }
+  }
+
+  function monta_tabela(clientes){
+    var table
+    table = `
+    <table class="table table-striped" style="color: black">
+  <thead >
+    <tr style="text-align:center;">
+      <th scope="col">Nome</th>
+      <th scope="col">Página</th>
+      <th scope="col">Caderno</th>
+    </tr>
+  </thead>
+  <tbody>
+    `    
+    clientes.forEach(element => {
+        table +=` 
+                <tr style="text-align:center;">
+                <td>`+element.nome_cliente+`</td>
+                <td>`+element.pagina_cliente+`</td>
+                <td>`+element.caderno_cliente+`</td>
+                </tr>
+                `
+        
+    });
+      
+    table +=` 
+    </tbody>
+</table>
+    `
+
+    document.getElementById("demo").innerHTML = table
+  }
