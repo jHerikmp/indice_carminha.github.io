94add1d7b2497b0dcfc6472625cb7d618a1fbbbf
Herik Mitre <33600632+jHerikmp@users.noreply.github.com>
2024-02-10 21:47:57 -0300

    update

---
+++ transforma_em_json.php
@@ -0,0 +1,29 @@
+<?php
+
+$string = trim(file_get_contents("texto_para_converter.txt"));
+
+$array_string = explode('|', $string);
+//echo "<pre>"; print_r($array_string);
+$contador = 0;
+$array_organizada = [];
+foreach($array_string as $key => $string){
+    $contador++;
+    if($contador == 3){
+        array_push($array_organizada, array(
+            "nome_cliente" => trim($array_string[$key - 2]),
+            "pagina_cliente" => trim($array_string[$key - 1]),
+            "caderno_cliente" => trim($array_string[$key]),
+        ));
+        $contador = 0;
+    }
+}
+
+echo "["."<br>";
+foreach ($array_organizada as $organizado){
+    //echo '{"nome_cliente": "'+$organizado["nome_cliente"]+'", "pagina_cliente": '+$organizado["pagina_cliente"]+', "caderno_cliente": "'+$organizado["caderno_cliente"]+'"},'."<br>";
+    echo '{"nome_cliente": "'.$organizado["nome_cliente"].'", "pagina_cliente": '.$organizado["pagina_cliente"].', "caderno_cliente": "'.$organizado["caderno_cliente"].'"},'."<br>";
+}
+echo "]";
+
+
+
