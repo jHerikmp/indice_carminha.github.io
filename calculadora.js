c798fba7a03e672d3ee1257321151fe3dfd76e38
Herik Mitre <33600632+jHerikmp@users.noreply.github.com>
2024-02-11 00:46:49 -0300

    fix

--- calculadora.js
+++ calculadora.js
@@ -4,6 +4,7 @@ var total = Number(0)
 
 function operacao_conta(){
   var input = document.getElementById("operacao").value
+  if(!input) return false
   /*/**/ 
   const soma = input.split("+");
   const subtracao = input.split("-");
@@ -14,36 +15,36 @@ if(soma.length >= 2){
   valor = Number(soma[0].replace(/,/g, '.')) + Number(soma[1].replace(/,/g, '.'))
   total += Number(valor)
   document.getElementById("historico").innerHTML += `
-  <h4>`+soma[0]+` + `+soma[1]+` = `+valor+`</h4><br>
+  <h6>`+soma[0]+` + `+soma[1]+` = `+valor+`</h6><br>
   `
 }else if(subtracao.length >= 2){
   valor = Number(subtracao[0].replace(/,/g, '.')) - Number(subtracao[1].replace(/,/g, '.'))
   total += Number(valor)
   document.getElementById("historico").innerHTML += `
-  <h4>`+subtracao[0]+` - `+subtracao[1]+` = `+valor+`</h4><br>
+  <h6>`+subtracao[0]+` - `+subtracao[1]+` = `+valor+`</h6><br>
   `
 }else if(divisao.length >= 2){
   valor = Number(divisao[0].replace(/,/g, '.')) / Number(divisao[1].replace(/,/g, '.'))
   total += Number(valor)
   document.getElementById("historico").innerHTML += `
-  <h4>`+divisao[0]+` / `+divisao[1]+` = `+valor+`</h4><br>
+  <h6>`+divisao[0]+` / `+divisao[1]+` = `+valor+`</h6><br>
   `
 }else if(multiplicacao.length >= 2){
   valor = Number(multiplicacao[0].replace(/,/g, '.')) * Number(multiplicacao[1].replace(/,/g, '.'))
   total += Number(valor)
   document.getElementById("historico").innerHTML += `
-  <h4>`+multiplicacao[0]+` * `+multiplicacao[1]+` = `+valor+`</h4><br>
+  <h6>`+multiplicacao[0]+` * `+multiplicacao[1]+` = `+valor+`</h6><br>
   `
 }else{
   valor = Number(input.replace(/,/g, '.'))
   total += Number(valor)
   document.getElementById("historico").innerHTML += `
-  <h4> +`+valor+`</h4><br>
+  <h6> +`+valor.toFixed(2)+`</h6><br>
   `
 }
   /**/
   document.getElementById("total").innerHTML = `
-  <h3>Total da Operação: R$ `+total.toFixed(2)+`</h3><br><hr>  `
+  <h4>Total da Operação: R$ `+total.toFixed(2)+`</h4><br><hr>  `
 
   document.getElementById("operacao").value = ''
   
@@ -54,5 +55,14 @@ if(soma.length >= 2){
     document.getElementById("operacao").value = ''
     document.getElementById("historico").innerHTML = ''
     document.getElementById("total").innerHTML = ''
+  }
+
+  function adicionaSinal(valor){
+    if(valor != "limpa"){
+      document.getElementById("operacao").value += valor
+      document.getElementById("operacao").focus()
+    }else{
+      document.getElementById("operacao").value = ''
+    }
   }
 
